e5c65bfd7b6840be644e75c1fcb7d142483abe9d
Edwin Ramirez <edwin.sti.ramirez09@gmail.com>
2024-01-19 21:11:10 -0500

    app tareas version movil - implementando funcionalidad iconos - tema

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react"
 import LunaIcon from "../icons/LunaIcon"
+import SolIcon from "../icons/SolIcon"
 
 const Header = () => { 
+    
+    const [darkMode, setDarkMode] = useState(false)
+
+    useEffect(() => {
+        if(darkMode){
+            document.documentElement.classList.add('dark')
+        }
+        else{
+            document.documentElement.classList.remove('dark')
+        }
+    }, [darkMode])
+    
+
     return (
     <header className="container mx-auto px-4 pt-8 ">
     <div className="flex justify-between">
     <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.4em]">Tareas</h1>
-    <button><LunaIcon className="fill-red-400" /></button>
+    <button onClick={()=>setDarkMode(!darkMode)}>
+        {
+            darkMode ?   <SolIcon/> : <LunaIcon/>
+        }
+    </button>
     </div>
     </header>
     )
