91d3c8d32a32c53730d8bd8697b71f416a52c941
Edwin Ramirez <edwin.sti.ramirez09@gmail.com>
2024-01-26 19:25:51 -0500

    Prueba

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Header from "./components/Header"
 import ListaTareas from "./components/ListaTareas"
 import TareasComputadas from "./components/TareasComputadas"
 
-const estadoInicialTareas = JSON.parse(localStorage.getItem("tareas")|| [] )
+const estadoInicialTareas = JSON.parse(localStorage.getItem("tareas") || [] )
 
 const App = () => {
   
     const [tareas, setTareas] = useState(estadoInicialTareas)
 
     useEffect(() => {
-      localStorage.setItem("tareas",JSON.stringify(tareas))
+        localStorage.setItem("tareas",JSON.stringify(tareas))
     }, [tareas])
     
 
@@ -52,10 +52,10 @@ const App = () => {
     }
   }
 
-  return (<div className="bg-[url('./assets/images/bg-mobile-light.jpg')] dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] bg-no-repeat bg-contain min-h-screen bg-gray-300 dark:bg-gray-900 dark:text-gray-200 transition-all duration-1000">
-    {/*Header*/}
+  return (<div className="bg-[url('./assets/images/bg-mobile-light.jpg')] dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] bg-no-repeat bg-contain min-h-screen bg-gray-300 dark:bg-gray-900 dark:text-gray-200 transition-all duration-1000">
+    {/*Header md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]*/}
     <Header/>
-    <main className="container mx-auto px-4 mt-8 md:max-w-5xl">
+    <main className="container mx-auto px-4 mt-8 md:max-w-5xl  "> 
       {/*Crear Tareas*/}
       <CrearTarea crearTarea={crearTarea}/>
       {/*TareaList Lista de todas las tareas ItemTarea -> articulos*/}
